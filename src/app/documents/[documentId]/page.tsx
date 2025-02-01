569a1dce51cd13e330945bb1f02de0f2dd91db15
ashish000777 <ashishbagate03@gmail.com>
2025-02-01 13:45:07 +0530

    Tiptap editor

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,3 +1,5 @@
+import { Editor } from "./editor";
+
 interface DocumentIdPageProps {
     params: Promise<{ documentId: string}>;
 };
@@ -5,8 +7,8 @@ interface DocumentIdPageProps {
 const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
     const { documentId } = await params;
     return ( 
-        <div>
-            Document ID: {documentId}
+        <div className="min-h-screen bg-[#FAFBFD]">
+            <Editor />
         </div>
      );
 }
